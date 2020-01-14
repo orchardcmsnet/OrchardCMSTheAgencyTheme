2d32d8d05ad6a61ca41c2dd59a949c50bc61f132
orchardcmsnet <blazor@orchardcms.net>
2020-01-13 20:59:49 -0700

    Added OrchardCMS Agency

---
+++ TheAgencyTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Orchard CMS Agency Theme",
+    Author = "OrchardCMS.net",
+    Website = "https://OrchardCMS.net",
+    Version = "2.0.0",
+    Description = "A theme adapted for agency websites."
+)]
